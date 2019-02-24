d9b5d8256f3fbce0036a1da6fca4e0eea5d817e9
Joe Hong <jhng323@gmail.com>
2019-02-24 09:34:58 -0500

    initial commit

---
+++ src/main/java/com/example/basicspringbootproject/BasicSpringbootProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.basicspringbootproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BasicSpringbootProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BasicSpringbootProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/basicspringbootproject/BasicSpringbootProjectApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.basicspringbootproject;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BasicSpringbootProjectApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
